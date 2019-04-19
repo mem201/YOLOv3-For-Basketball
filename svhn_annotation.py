066285d49486f6c8ce80029bb9717b984bff422b
AnnHunter <44405565+AnnHunter@users.noreply.github.com>
2019-04-20 02:30:34 +0800

    Update svhn_annotation.py

--- svhn_annotation.py
+++ svhn_annotation.py
@@ -7,6 +7,7 @@ classes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
 def convert_annotation(image_set,image_id, list_file):
+    #where the annotation of the dataset at
     an_dir ='/home/ang/pywork/svhn/annotation/%s/%s.xml'%(image_set,image_id)
     in_file = open(an_dir)
     tree=ET.parse(in_file)
@@ -23,8 +24,12 @@ def convert_annotation(image_set,image_id, list_file):
         list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
 
 for  image_set in sets:
+    #your dataset direction
     image_dir="/home/ang/pywork/svhn/image/%s/"%image_set
+    
+    #where you save the text file
     txt_dir='/home/ang/pywork/svhn/%s.txt'%image_set
+    
     list_file=open(txt_dir,"w")
     file_name=[]
     for root, dirs, files in os.walk(image_dir) :
