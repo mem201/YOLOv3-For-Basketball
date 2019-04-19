2a708c480c597619b20b9d19e6ae1afdd056a475
AnnHunter <44405565+AnnHunter@users.noreply.github.com>
2019-04-20 02:25:12 +0800

    Update svhn_annotation.py

--- svhn_annotation.py
+++ svhn_annotation.py
@@ -1,13 +1,13 @@
 import xml.etree.ElementTree as ET
 import os 
 
-sets=['train',  'test']
+sets=['train', 'test']
 
 classes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
 def convert_annotation(image_set,image_id, list_file):
-    an_dir='/home/ang/pywork/svhn/annotation/%s/%s.xml'%(image_set,image_id)
+    an_dir ='/home/ang/pywork/svhn/annotation/%s/%s.xml'%(image_set,image_id)
     in_file = open(an_dir)
     tree=ET.parse(in_file)
     root = tree.getroot()
@@ -23,19 +23,19 @@ def convert_annotation(image_set,image_id, list_file):
         list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
 
 for  image_set in sets:
-    image_dir="/home/ang/pywork/svhn/image/%s"%image_set
+    image_dir="/home/ang/pywork/svhn/image/%s/"%image_set
     txt_dir='/home/ang/pywork/svhn/%s.txt'%image_set
     list_file=open(txt_dir,"w")
-
+    file_name=[]
     for root, dirs, files in os.walk(image_dir) :
-         file_min=min(files)
-         png=file_min.find('.png')
-         num_min=int(file_min[0:png])
-         num=len(files)
+        for a in files:
+          file_name.append(a)
 
-    for image_id in range(num_min,num_min+num-2):
-        list_file.write('/home/ang/pywork/svhn/image/%s/%s.png'%(image_set, image_id))
-        convert_annotation(image_set,image_id, list_file)
+    for image_id in file_name:
+        num_png=image_id.find('.png')
+        file=str(image_id[0:num_png])
+        list_file.write(str(image_dir+image_id))
+        convert_annotation(image_set, file, list_file)
         list_file.write('\n')
         
     list_file.close()
